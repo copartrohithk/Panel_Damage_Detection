2d92f542d3ad230bb6d04db5390e7e99ec7ec3be
copartrohithk <rohith.kokkula@copart.com>
2024-06-25 16:01:47 -0500

    Trying out object detection using YOLO purpose is to learn and implement an end-to-end ml framework -Trained my first model today -Will try to use GCP to run it next time

--- train.py
+++ train.py
@@ -1,2 +1,10 @@
 import numpy as np
 import pandas as pd
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="config.yaml", epochs=1,device="mps")  # train the model
