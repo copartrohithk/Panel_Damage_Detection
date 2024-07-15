9cd851b374d578322fffc3bb74b599e566f162f4
copartrohithk <rohith.kokkula@copart.com>
2024-07-15 10:46:16 -0500

    Trying out object detection using YOLO purpose is to learn and implement an end-to-end ml framework -Trained my first model today -Will try to use GCP to run it next time -create data splits the downloaded file into train-test-val split -added the gcs helper function -added the flask file

---
+++ helloflask/main.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def my_form():
+    if request.method == 'POST':
+        text = request.form['text']
+        processed_text = text.upper()
+        return processed_text
+    return render_template('template.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
