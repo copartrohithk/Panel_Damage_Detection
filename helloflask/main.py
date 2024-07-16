88eb9502a4ecb13909a4fad575b58fb6a018bf02
copartrohithk <rohith.kokkula@copart.com>
2024-07-16 12:05:03 -0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	requirements.txt

--- helloflask/main.py
+++ helloflask/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, render_template
+import train
 
 app = Flask(__name__)
 
@@ -10,6 +11,8 @@ def my_form():
         processed_text = text.upper()
         return processed_text
     return render_template('template.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- train.py
+++ train.py
@@ -2,9 +2,25 @@ import numpy as np
 import pandas as pd
 
 from ultralytics import YOLO
+import torch
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+# model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=1,device="mps")  # train the model
+# model.train(data="config.yaml", epochs=1,device="mps")  # train the model
+
+model = YOLO('/Users/rokokkula/Documents/Panel_Damage_Detection/models/panel_best.pt')
+dmg_model = YOLO('/Users/rokokkula/Documents/Panel_Damage_Detection/models/dmg_best.pt')
+
+
+def predict(link):
+    model.predict(link,
+                  save=True, conf=0.9, project="test_output/panel")
+    dmg_model.predict(link,
+                      save=True, conf=0.1, project="test_output/dmg")
+
+def output():
+    x="/Users/rokokkula/Documents/Panel_Damage_Detection/test_output/dmg/predict/8fceef3db9824c4ba19ead5814eb7eb1_ful.jpg"
+    y="/Users/rokokkula/Documents/Panel_Damage_Detection/test_output/panel/predict/8fceef3db9824c4ba19ead5814eb7eb1_ful.jpg"
+    return x,y
